276a295f4abb1c3f06f262afc9338ce980a7236d
Excaling <slendermandu31770@outlook.com>
2018-09-02 14:57:23 +0200

    Creation de message d'erreur | Close #6

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -5,6 +5,23 @@ class UtilisateursController < ApplicationController
  
   def show
     @utilisateur = Utilisateur.find(1) #ou find(params[:id])
-  
   end
+
+  def create
+    
+    @utilisateur = Utilisateur.new(utilisateur_params)
+    if @utilisateur.save
+      #sauvegarde confirme
+      flash[:success] = "Bienvenue"
+      redirect_to @utilisateur
+    else
+      render "new"
+    end
+
+  end
+
+  private
+    def utilisateur_params
+      params.require(:utilisateur).permit(:nom, :email, :password, :password_confirmation)
+    end
 end
